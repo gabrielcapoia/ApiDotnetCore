d67d99bbb208304040861140eac203dbe470b20e
gabrielcapoia <gabriel.capoia@dbdiagnosticos.com.br>
2022-03-21 17:02:02 -0300

    Configure dbContext and create Fornecedor Model

--- ApiDotnetCore/Startup.cs
+++ ApiDotnetCore/Startup.cs
@@ -1,3 +1,4 @@
+using ApiDotnetCore.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -25,6 +27,11 @@ namespace ApiDotnetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApiDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddControllers();
         }
 
