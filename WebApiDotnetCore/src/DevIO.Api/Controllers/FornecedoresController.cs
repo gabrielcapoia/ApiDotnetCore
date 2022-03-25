f44fe93f1bedbb2c1d60c2d1dbeb74177561f4af
Gabriel Capoia <gcapoia@gmail.com>
2022-03-25 08:18:00 -0300

    Add DI config and Automapper

---
+++ WebApiDotnetCore/src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]    
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository fornecedorRepository;
+        private readonly IMapper mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+            IMapper mapper)
+        {
+            this.fornecedorRepository = fornecedorRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedores = await fornecedorRepository.ObterTodos();
+            return mapper.Map<IEnumerable<FornecedorViewModel>>(fornecedores);
+        }
+    }
+}
